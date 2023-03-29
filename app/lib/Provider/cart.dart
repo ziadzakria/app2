ca833997f4b8617c21586251a42c60701426647c
ziadzakria <94961797+ziadzakria@users.noreply.github.com>
2023-03-29 19:58:27 +0200

    checkout screen

--- app/lib/Provider/cart.dart
+++ app/lib/Provider/cart.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 
 class cart with ChangeNotifier {
   List selectedProducts = [];
-
+  int price = 0;
   add(Item prodact) {
     selectedProducts.add(prodact);
+    price += prodact.price.round();
+    notifyListeners();
   }
 }
